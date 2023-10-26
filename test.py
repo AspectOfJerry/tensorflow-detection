23df425ff975ae610fb93aefc42a34b93a9f4f4d
Jerry <jerryfu_mc@outlook.com>
2023-10-26 19:02:51 -0400

    heh?

--- test.py
+++ test.py
@@ -1,48 +1,7 @@
-import tensorflow as tf
 from tensorflow import keras
 
-from custom_dataset import CustomDataset
-from utils import log, Ccodes
 
-# Constants
-DATASET_DIR = "./dataset"
-OUTPUT_DIR = "./output"
-
-# NUM_CLASSES = 3  # Number of classes, including the background class (+1) <- ?
-NUM_CLASSES = 2
-INPUT_SHAPE = (224, 224)
-
-NUM_EPOCHS = 16
-BATCH_SIZE = 32
-LABEL_MAP = {
-    "background": 0,
-    "cube": 1,
-    "cone": 2,
-}
-
-log(f"Training configuration:"
-    f"\n\t- Number of classes: {NUM_CLASSES}"
-    f"\n\t- Input size: {INPUT_SHAPE}"
-    f"\n\t- Number of epochs: {NUM_EPOCHS}"
-    f"\n\t- Batch size: {BATCH_SIZE}"
-    f"\n\t- Label map: {LABEL_MAP}", Ccodes.BLUE)
-
-train_dataset = CustomDataset(DATASET_DIR, "train", INPUT_SHAPE, BATCH_SIZE, LABEL_MAP)
-
-log(f"Number of training images: {len(train_dataset)}", Ccodes.GREEN)
-
-
-# Define the model architecture (based on MobileNet)
-def custom_model(input_shape, num_classes):
-    NUM_SCALES = 3
-    NUM_ASPECT_RATIOS = 3
-    NUM_LOCATIONS = 64 * 64
-    NUM_ANCHORS = NUM_SCALES * NUM_ASPECT_RATIOS * NUM_LOCATIONS
-
-    # TODO: Add anchor box generation and other object detection components here
-
-    # Backbone (Convolutional Base)
-    input_tensor = keras.layers.Input(shape=input_shape)
+def custom_model():
     backbone = keras.Sequential([
         keras.layers.Conv2D(32, (3, 3), strides=2, activation="relu", input_shape=(224, 224, 3)),  # 1
         keras.layers.DepthwiseConv2D((3, 3), strides=1, activation="relu", padding="same"),  # 2
@@ -74,39 +33,17 @@ def custom_model(input_shape, num_classes):
         keras.layers.AveragePooling2D((7, 7), strides=1, padding="valid"),  # 28
         keras.layers.Flatten(),  # 29
         keras.layers.Dense(1024, activation="relu"),  # 29 (fully connected)
-        keras.layers.Dense(NUM_CLASSES, activation="softmax")  # 30 (Softmax), Classifier
+        keras.layers.Dense(2, activation="softmax")  # 30 (Softmax), Classifier
     ])
 
     detection_head = keras.Sequential([
-        keras.layers.Conv2D(num_classes, (3, 3), activation="relu", padding="same"),
+        keras.layers.Conv2D(2, (3, 3), activation="relu", padding="same"),
         # Add more convolutional layers for object detection as needed
     ])
 
     return model
 
 
-# Create the model
-# model = custom_model_sequential((224, 224, 3), NUM_CLASSES)
-model = custom_model((224, 224, 3), NUM_CLASSES)
+model = custom_model()
 
-
-# Custom loss function
-def custom_loss(y_true, y_pred):
-    # TODO: Implement custom loss function
-    return  # loss
-
-
-# Compile the model
-model.compile(optimizer="adam")
-
-# Training
-model.fit(train_dataset, epochs=NUM_EPOCHS)
-
-# Save the model
-model.save(OUTPUT_DIR + "custom_model_sequential.h5")
-
-# Inference
-# loaded_model = keras.models.load_model(OUTPUT_DIR + "cortex_model.h5")
-
-# Predict
-# predictions = loaded_model.predict(new_data)
+model.summary()
